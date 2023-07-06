806071e412637fdaa64e86f034c2032fb1b2436f
JNLN01 <j4cob_@hotmail.co.uk>
2023-07-06 17:38:21 +0200

    Added user options to save the dataframe as .csv

--- data injector.py
+++ data injector.py
@@ -12,4 +12,18 @@ weekly_df1 = df1.resample("W").agg({'Open': 'first', 'Close': 'last'})
 weekly_df1.head() #Above, filters data to weekly. Sunday open to Friday close.
 print(weekly_df1.head) #prints the table for x instrument for y dates
 
-#create option to save / download dataframe.
+#To be worked on; Rework options for the dataframe + manipulation options of said dataframe.
+#User option to save the dataframe.
+
+var = input("Do you want to save? ")
+var = var.lower()   #forces user response to be lowercase.
+
+if var == 'yes':
+    df1.to_csv('yahoo_dataframe.csv')
+    print("Dataframe saved as yahoo_dataframe.csv")
+
+elif var == 'no':
+   print("Not saved as CSV")
+
+else:
+    print('Incorrect Input. Try a yes or no')
