41f859a4b55b1c3d17ca9aa4c619934e87f3f53e
JNLN01 <j4cob_@hotmail.co.uk>
2023-07-08 18:42:13 +0200

    Added some new comments - To make it clear what the code is.
    Changed var to save variable

--- data injector.py
+++ data injector.py
@@ -5,24 +5,25 @@ import pandas as pd
 def download(ticker, start_date, end_date):
     df = yf.download(ticker, start=start_date, end=end_date)
     return df
-
+#                Asset   ,Start Date  , End Date
 df1 = download("EURUSD=X", "2003-12-1", "2023-03-06",)
 df1 = df1.loc[:, ["Open", "Close"]]  # Sets out only Open and close prices in the dataset.
 weekly_df1 = df1.resample("W").agg({'Open': 'first', 'Close': 'last'})
 weekly_df1.head() #Above, filters data to weekly. Sunday open to Friday close.
-print(weekly_df1.head) #prints the table for x instrument for y dates
+print(weekly_df1.head) #prints the dataframe for x instrument for y date range
 
 #To be worked on; Rework options for the dataframe + manipulation options of said dataframe.
+
 #User option to save the dataframe.
 
-var = input("Do you want to save? ")
-var = var.lower()   #forces user response to be lowercase.
+save = input("Do you want to save? ")
+save = save.lower()   #forces user response to be lowercase.
 
-if var == 'yes':
+if save == 'yes':
     df1.to_csv('yahoo_dataframe.csv')
     print("Dataframe saved as yahoo_dataframe.csv")
 
-elif var == 'no':
+elif save == 'no':
    print("Not saved as CSV")
 
 else:
